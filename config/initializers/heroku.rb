8ae6d330c24b24afade1dd19d7921af7489f592f
GriwMF <learningisalife@gmail.com>
2018-12-25 13:33:00 +0200

    heroku autoping

--- config/initializers/heroku.rb
+++ config/initializers/heroku.rb
@@ -1,10 +1,10 @@
 require 'open-uri'
 
-if Rails.env.production?
+if Rails.env.production? && !$PROGRAM_NAME =~ /rake/
   Thread.new do
     loop do
       open('http://spacycrew.com').status
-      sleep 3600
+      sleep 600
     end
   end
 end
