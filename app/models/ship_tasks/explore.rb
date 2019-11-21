a8632365712431ee6f437353ff219ca8b8fff9ee
GriwMF <learningisalife@gmail.com>
2019-11-21 20:35:24 +0200

    reverted ship actions change

--- app/models/ship_tasks/explore.rb
+++
@@ -1,9 +0,0 @@
-module ShipTasks
-  class Explore < Processor
-    def step
-      amount = ship.credits.amount + 10
-      ship.credits.update!(amount: amount)
-      History.create!(object: ship, action: :explore, params: { credits: amount })
-    end
-  end
-end
