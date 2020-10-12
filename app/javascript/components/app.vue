7a3ea1e414b9f6ef24088fc21b9109872994b3b1
GriwMF <learningisalife@gmail.com>
2020-10-12 10:49:39 +0300

    ships component

--- app/javascript/components/app.vue
+++ app/javascript/components/app.vue
@@ -1,51 +1,14 @@
 <template>
   <div id="app">
-    <a href="#" @click.prevent="pingServerGo()">Go!</a>
-    <history-filter/>
-    <ul>
-      <li v-for="history in filteredHistories" :key="history.id">
-        {{ formattedHistory(history) }}
-      </li>
-    </ul>
+    <ships/>
   </div>
 </template>
 
 <script>
-import { mapGetters, mapMutations } from 'vuex'
-import historyFilter from './historyFilter'
-import { createConsumer } from "@rails/actioncable"
-
+import History from './history'
+import Ships from './ships'
 export default {
-  created () {
-    let cable = createConsumer('ws://localhost:3000/cable');
-
-    cable.subscriptions.create('HistoryChannel', {
-      received: this.appendHistory
-    })
-  },
-  components: {historyFilter},
-  computed: mapGetters([
-    // проксирует в this.count доступ к store.state.count
-    'filteredHistories'
-  ]),
-  methods: {
-    ...mapMutations(['appendHistory']),
-    replacer: function (key, value) {
-      // Фільтрація властивостей
-      if (key === 'updated_at' || key === 'created_at') {
-        return undefined;
-      }
-      return value;
-    },
-
-    formattedHistory: function (history) {
-      return JSON.stringify(history, this.replacer)
-    },
-
-    pingServerGo: function () {
-      fetch('http://localhost:3000/bc');
-    }
-  }
+  components: {Ships, History}
 }
 </script>
 
---
+++ app/javascript/components/ships.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="grid">
+    <ul>
+      <li>Ship1</li>
+      <li>Ship2</li>
+    </ul>
+
+    <div>
+      System 1
+      System 2
+    </div>
+
+    <div>
+      System info
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ships'
+}
+</script>
+
+<style scoped>
+  .grid {
+    display: grid;
+    grid-template-rows: 1fr 1fr;
+    grid-template-columns: 1fr 1fr 3fr;
+  }
+
+  li {
+    list-style-type: none;
+  }
+</style>
